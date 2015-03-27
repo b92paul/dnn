cc7bab0d2282c99d598658f4649d9e674bcb31b7
b92paul <b92paul@gmail.com>
2015-03-27 17:05:54 +0800

    dnn.cpp add SGD, update function, modify contructor

--- hw1/dnn.cpp
+++ hw1/dnn.cpp
@@ -5,7 +5,7 @@
 
 using namespace Eigen;
 using namespace std;
-
+typedef vector<VectorXd> VXd;
 
 VectorXd sigmoid(VectorXd x)
 {
@@ -21,10 +21,11 @@ class NetWork
 		int input_length;//input x vector_length 
 		int layers;
 		int *neuron;
+		int input_size;
 		VectorXd *bias;
 		MatrixXd *weight;
 		
-		NetWork(vector<int>Neuron)
+		NetWork(vector<int>Neuron, int _input_size):input_size(_input_size)
 		{
 				layers = Neuron.size();
 				neuron = new int[layers];
@@ -48,8 +49,6 @@ class NetWork
 		}
 		void back_propgation(VectorXd x,VectorXd y,VectorXd *delta_b,MatrixXd *delta_w)
 		{
-				delta_b = new VectorXd[layers];
-				delta_w = new MatrixXd[layers];
 				vector<VectorXd>activation,zs;
 				activation.push_back(x);
 				for(int i=0;i<layers;i++) 
@@ -61,15 +60,47 @@ class NetWork
 				}
 
 				VectorXd d= cost_derivative(x,y).cwiseProduct(sigmoid_prime(zs[layers-1]));
-				delta_b[layers-1] = d;
-				delta_w[layers-1] = d*(x.transpose());
+				delta_b[layers-1] += d;
+				delta_w[layers-1] += d*(x.transpose());
 				for(int l=2;l<=layers;l++)
 				{
 						VectorXd spv = sigmoid_prime(zs[layers-l]);
 						d = (weight[layers-l].transpose()*d).cwiseProduct(spv); 
-						delta_b[layers-l] = d;
-						delta_w[layers-l] = d*(activation[layers+1-l].transpose());
+						delta_b[layers-l] += d;
+						delta_w[layers-l] += d*(activation[layers+1-l].transpose());
 				}
 		}
 		VectorXd cost_derivative(VectorXd output,VectorXd y){return output-y;}
+		void SGD(VXd& TrainX, VXd& TrainY, double eta, int epochs, int msize,VXd& ValX, VXd& ValY ){
+			int count =0 ;
+			VXd x,y;
+			for(int i=0; i<epochs; i++){
+				if(count+msize >= TrainX.size())count=0;
+				x = VXd(TrainX.begin()+count,TrainX.begin()+count+msize);
+				y = VXd(TrainY.begin()+count,TrainY.begin()+count+msize);
+				count+=msize;
+				update(x,y,eta);
+				printf("epoch %d\n",i+1);
+			}
+		}
+		void update(VXd& bX, VXd& bY,double eta){
+				double msize = (double)bX.size();
+				VectorXd* delta_b = new VectorXd[layers];
+				for(int i=0;i<layers;i++)delta_b[i] = VectorXd::Zero(neuron[i]);
+				MatrixXd* delta_w = new MatrixXd[layers];
+				
+				for(int i=0;i<layers;i++){
+					if(i==0) delta_w[i] = MatrixXd::Zero(input_size,neuron[i]);
+					else delta_w[i] = MatrixXd::Zero(neuron[i-1],neuron[i]);
+				}
+				for(int i=0;i<bX.size();i++){
+					back_propgation(bX[i],bY[i],delta_b,delta_w);
+				}
+				for(int i=0;i<layers;i++){
+					bias[i] -= eta*delta_b[i]/msize;
+					weight[i] -= eta*delta_w[i]/msize;	
+				}
+		}
+		double eval(VXd ValBatchX){return 0.0;}
+
 };
