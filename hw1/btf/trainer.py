8419463bdecb284af7b11eaeba0d632ca88668b4
Po-Cheng Chu <darkbtf@gmail.com>
2015-03-29 02:17:52 +0800

    train

--- hw1/btf/trainer.py
+++ hw1/btf/trainer.py
@@ -2,29 +2,30 @@
 from dnn import DeepNeuralNetwork
 from data_processor import TrainDataProcessor
 
+import sys
+
 class Trainer:
 	def __init__(self):
 
 		self.model = DeepNeuralNetwork()
 		self.model.init(input_count = 108,
-										hidden_layer_numbers = [128],
+										hidden_layer_numbers = [80, 80],
 										output_count = 48,
 										learning_rate = 0.05)
 		self.data = TrainDataProcessor(-1)
 
-	def run(self):
-		self.data.set_cursor(200000)
+	def run(self, batch = 2000):
 		count = 0
-		while True:
+		while count < batch:
 			count += 1
-			record = self.data.get_record()
+			record = self.data.get_train_record()
 			if record is None:
 				break
 			#print record
 			self.model.forward(record[0])
 			self.model.backpropagation(record[1])
 			self.model.update()
-			print str(count) + ' data entries processed.'
+			#print str(count) + ' data entries processed.'
 
 	def get_index(self, result):
 		for i in xrange(len(result)):
@@ -32,38 +33,42 @@ class Trainer:
 				return i
 		return -1
 
-	def validate(self):
-		self.data.set_cursor(0)
-		VALID_COUNT = 200000
+	def validate(self, batch=10000):
 		count = 0
 		correct = 0
-		while count < VALID_COUNT:
+		while count < batch:
 			count += 1
-			record = self.data.get_record()
+			record = self.data.get_valid_record()
 			if record is None:
 				break
 			#print record
 			result = self.model.predict(record[0])
 			answer = self.get_index(record[1])
-			print '# ' + str(count) + ': ' + str(result) + ' ' + str(answer)
+			#print '# ' + str(count) + ': ' + str(result) + ' ' + str(answer)
 			if result == answer:
 				correct += 1
-		print 'validation: ' + str(float(correct) / float(VALID_COUNT))
+		print 'validation: ' + str(float(correct) / float(batch))
 
-	def save(self, filename = 'train.config'):
+	def save(self, filename = 'train2.config'):
 		self.model.save_model(filename)
 
 	def load(self, filename = 'train.config'):
 		self.model.load_model(filename)
 
 def main():
+	
 	trainer = Trainer()
-	EPOCH = 1
-	trainer.load()
+	EPOCH = 1000
+	trainer.load(sys.argv[1])
 	for i in xrange(EPOCH):
+		print 'epoch # ' + str(i)
 		trainer.run()
-	trainer.validate()
-	trainer.save()
+		if i % 25 == 0:
+			trainer.validate()
+	trainer.validate(200000)
+	trainer.save(sys.argv[2])
+	print "parameter saved"
+
 
 if __name__ == '__main__':
 	main()
