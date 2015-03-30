a7b797a7ec3982f8c40191864db5fb6bceba3246
b92paul <b92paul@gmail.com>
2015-03-31 05:09:36 +0800

    add fast_dnn.cpp, speed up not done but could run.

--- hw1/main.cpp
+++ hw1/main.cpp
@@ -1,12 +1,12 @@
 #include <cstdio>
 #include <Eigen/Dense>
 #include <vector>
-#include "dnn_fast.cpp"
+#include "fast_dnn.cpp"
 #include <random>
 using namespace std;
 using Eigen::MatrixXd;
 using Eigen::VectorXd;
-char labelPath[] = "../../data/merge/label.out";
+char labelPath[] = "../../data/merge/label_39.out";
 char trainPath[] = "../../data/merge/train.out";
 char testPath[]  = "../../data/merge/test.out";
 char testId[]    = "../../data/merge/test_id.out";
@@ -14,7 +14,7 @@ char testId[]    = "../../data/merge/test_id.out";
 typedef vector<double> VD;
 typedef vector<VectorXd> VXd;
 int check = 100000;
-vector<VectorXd> csvToVecters(char* filename, int cut=300000){
+vector<VectorXd> csvToVecters(char* filename, int cut= -1){
 	int idx =0 ;
 	vector<VectorXd> res;
 	printf("%s\n",filename);
@@ -52,7 +52,7 @@ int main(){
 	printf("data size = %lu\n",inputX.size());
 	vector<VectorXd> inputY = csvToVecters(labelPath);
 	printf("data size = %lu\n",inputY.size());
-	int val_size = 5000;
+	int val_size = 10000;
 	printf("X size = %lu\n",inputX[0].size());
 	printf("Y size = %lu\n",inputY[0].size());
 	for(int i=0;i<inputX.size();i++)idx.push_back(i);
@@ -62,29 +62,36 @@ int main(){
 	double vnorm = 1;
 
 	VXd valX,valY,trainX,trainY;
-	for(int i=0;i<val_size;i++){
-		valX.push_back(inputX[idx[i]]/vnorm);
-		valY.push_back(inputY[idx[i]]/vnorm);
+	for(int i=0; i<val_size; i++){
+		valX.push_back(inputX[idx[i]]/ vnorm);
+		valY.push_back(inputY[idx[i]]/ vnorm);
 	}
-	for(int i = val_size; i<inputX.size();i++){
-		trainX.push_back(inputX[idx[i]]/vnorm);
-		trainY.push_back(inputY[idx[i]]/vnorm);
+
+	MatrixXd BX(inputX[0].size(),inputX.size() - val_size);
+	MatrixXd BY(inputY[0].size(),inputY.size() - val_size);
+	for(int i = val_size; i< inputX.size(); i++)
+	{
+			BX.col(i-val_size)<<inputX[idx[i]] / vnorm;
+			BY.col(i-val_size)<<inputY[idx[i]] / vnorm;
 	}
 
 	// new network
 	vector<int> layer;
-	layer.push_back(200);
-	layer.push_back(100);
-	layer.push_back(60);
+	layer.push_back(80);
+	layer.push_back(50);
+	layer.push_back(50);
+	layer.push_back(50);
+	layer.push_back(50);
 	layer.push_back(inputY[0].size());
 
 	int input_size = inputX[0].size();
 	NetWork nn(layer,input_size,0.9,true);
+	nn.outsize = 39;
 	
 	//read test data
 	vector<VectorXd> testX = csvToVecters(testPath);
 	printf("data size = %lu\n",testX.size());
-	nn.SGD(trainX,trainY,0.4 ,300000,500,valX,valY,testX);
+	nn.SGD(BX,BY,0.2 ,50000000,500,valX,valY,testX);
 
 	//nn.Predict(testX);
 	
