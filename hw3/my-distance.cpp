a78c0dbde54351f6e0da1cc976d857cb68c1048f
b92paul <b92paul@gmail.com>
2015-05-29 13:43:11 +0800

    change distance.cpp dir

---
+++ hw3/my-distance.cpp
@@ -0,0 +1,121 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <malloc.h>
+
+const long long max_size = 2000;         // max length of strings
+const long long N = 40;                  // number of closest words that will be shown
+const long long max_w = 50;              // max length of vocabulary entries
+
+int main(int argc, char **argv) {
+  FILE *f;
+  char st1[max_size];
+  char *bestw[N];
+  char file_name[max_size], st[100][max_size];
+  float dist, len, bestd[N], vec[max_size];
+  long long words, size, a, b, c, d, cn, bi[100];
+  char ch;
+  float *M;
+  char *vocab;
+  if (argc < 2) {
+    printf("Usage: ./distance <FILE>\nwhere FILE contains word projections in the BINARY FORMAT\n");
+    return 0;
+  }
+  strcpy(file_name, argv[1]);
+  f = fopen(file_name, "rb");
+  if (f == NULL) {
+    printf("Input file not found\n");
+    return -1;
+  }
+  fscanf(f, "%lld", &words);
+  fscanf(f, "%lld", &size);
+  vocab = (char *)malloc((long long)words * max_w * sizeof(char));
+  for (a = 0; a < N; a++) bestw[a] = (char *)malloc(max_size * sizeof(char));
+  M = (float *)malloc((long long)words * (long long)size * sizeof(float));
+  if (M == NULL) {
+    printf("Cannot allocate memory: %lld MB    %lld  %lld\n", (long long)words * size * sizeof(float) / 1048576, words, size);
+    return -1;
+  }
+  for (b = 0; b < words; b++) {
+    a = 0;
+    while (1) {
+      vocab[b * max_w + a] = fgetc(f);
+      if (feof(f) || (vocab[b * max_w + a] == ' ')) break;
+      if ((a < max_w) && (vocab[b * max_w + a] != '\n')) a++;
+    }
+    vocab[b * max_w + a] = 0;
+    for (a = 0; a < size; a++) fread(&M[a + b * size], sizeof(float), 1, f);
+    len = 0;
+    for (a = 0; a < size; a++) len += M[a + b * size] * M[a + b * size];
+    len = sqrt(len);
+    for (a = 0; a < size; a++) M[a + b * size] /= len;
+  }
+  fclose(f);
+  while (1) {
+    for (a = 0; a < N; a++) bestd[a] = 0;
+    for (a = 0; a < N; a++) bestw[a][0] = 0;
+    a = 0;
+    while (1) {
+      st1[a] = fgetc(stdin);
+      if ((st1[a] == '\n') || (a >= max_size - 1)) {
+        st1[a] = 0;
+        break;
+      }
+      a++;
+    }
+    if (!strcmp(st1, "EXIT")) break;
+    cn = 0;
+    b = 0;
+    c = 0;
+    while (1) {
+      st[cn][b] = st1[c];
+      b++;
+      c++;
+      st[cn][b] = 0;
+      if (st1[c] == 0) break;
+      if (st1[c] == ' ') {
+        cn++;
+        b = 0;
+        c++;
+      }
+    }
+    cn++;
+		// the part of our sim algorithm
+    for (a = 0; a < cn; a++) {
+      for (b = 0; b < words; b++) if (!strcmp(&vocab[b * max_w], st[a])) break;
+      if (b == words) b = -1;
+      bi[a] = b;
+    }
+		if(bi[0]==-1){puts("0.000000");continue;}
+    len =0;
+		for (a = 0; a < size; a++) vec[a] = M[a + bi[0]*size];
+    for (a = 0; a < size; a++) len += vec[a] * vec[a];
+    len = sqrt(len);
+    for (a = 0; a < size; a++) vec[a] /= len;
+    int bnum = 3;
+		double best[bnum]; // = -1,best2 = -1,best3 = -1,best4 = -1;
+		for(int i=0;i<bnum;i++) best[i] = -1;
+		double now = 0,n_len = 0;
+		for (b = 1; b < cn; b++) {
+      if (bi[b] == -1) continue;
+			now = 0, n_len = 0;
+      for (a = 0; a < size; a++){
+				now += vec[a] * M[a + bi[b] * size];
+				n_len += M[a+bi[b]*size] * M[a+bi[b]*size];
+			}
+			now /= sqrt(n_len);
+			for(int i=0;i<bnum;i++){
+				if(now > best[i]){
+					for(int j = bnum-1;j >i;j--)best[j] = best[j-1];
+					best[i] = now;
+					break;
+				}
+			}
+
+    }
+		double sum_best = 0;
+		for(int i=0;i < bnum; i++) sum_best+=best[i]*(10.0-i)/10.0;
+		printf("%lf\n",sum_best);
+  }
+  return 0;
+}
--- hw3/run.sh
+++ hw3/run.sh
@@ -1,6 +1,6 @@
 echo 'run parse_question.py'
 python parse_question_sim.py
 echo 'run word2vec and calc cos'
-./word2vec-read-only/distance word2vec-read-only/novel.bin < testing_parse.txt > cos.txt
+./my-distance word2vec-read-only/novel.bin < testing_parse.txt > cos.txt
 echo 'run cos_to_ans.py'
 python cos_to_ans.py
