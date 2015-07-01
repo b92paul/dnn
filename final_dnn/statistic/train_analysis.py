d5405fc5658508912644fb6d3ca130ba3412afee
b92paul <b92paul@gmail.com>
2015-07-01 23:37:40 +0800

    improve train analysis file

--- final_dnn/statistic/train_analysis.py
+++ final_dnn/statistic/train_analysis.py
@@ -5,6 +5,8 @@ train_file = open(dir_path + "f_train.ark")
 norm_file = open("preprocess/res.out","w")
 avg = [0.0 for _ in range(69)]
 res = [0.0 for _ in range(69)]
+Min = [1e6 for _ in range(69)]
+Max = [-1e6 for _ in range(69)]
 count = 0.0
 cut = -1
 w = True
@@ -24,10 +26,15 @@ for c,line in enumerate(train_file):
     x = float(x)
     res[i] += x**2
     avg[i] += x
+    Min[i] = min(Min[i], x)
+    Max[i] = max(Max[i], x)
   count += 1
 if w:
   for a, r in zip(avg,res):
     norm_file.write("%.8f %.8f\n" % (a/count,math.sqrt(r/count)) )
 else:
-  for a, r in zip(avg,res):
-    print("%.8f %.8f" % (a/count,math.sqrt(r/count)) )
+  for a, r, m, M in zip(avg,res,Min,Max):
+    ex = a/count
+    ex2 = r/count
+    var = math.sqrt(ex2-ex**2)
+    print("E[X]:%.8f Var(x):%.8f Min:%.8f Max:%.8f" % (a/count,var,m,M) )
